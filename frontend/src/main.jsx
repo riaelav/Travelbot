edd24de78f8b6f7041fd3c46717f8078df683ba3
Valeria Pagliarini <valepagliar@gmail.com>
2025-09-14 10:18:41 +0200

    Added basic css.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./index.css";
+import "./App.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
